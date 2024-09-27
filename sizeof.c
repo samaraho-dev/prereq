782fd0f851d6ed6f867048f936286f71d6a9d1c5
Samaraho Mukherjee <154082806+samaraho-dev@users.noreply.github.com>
2024-09-28 01:39:45 +0530

    1st Problem

---
+++ sizeof.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* Your job is to use the sizeof() operator to find the size of int, float, double and char.
+Compile it on C and Turbo C on onlinegdb.com*/
+int main() {
+  printf("%d %d %d %d", ______ , ______, ______, ______);
+  return 0;
+}
