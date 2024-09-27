0903af6e8ec912a901a0109eadae4b45f6638e1c
Samaraho Mukherjee <154082806+samaraho-dev@users.noreply.github.com>
2024-09-28 01:56:12 +0530

    5th Problem

---
+++ incdec.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void increment() {
+    //your code goes here
+}
+void decrement() {
+    //your code goes here  
+}
+
+int main() {
+    int x = 15;
+    int y = 20;
+    printf("Before %d %d\n", x,y);
+    increment(__);
+    decrement(__);
+    decrement(__);
+    printf("After %d %d\n", x,y);
+}
