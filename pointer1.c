a183ca62d71cfbddc337314c85c9057f833d676f
Samaraho Mukherjee <154082806+samaraho-dev@users.noreply.github.com>
2024-09-28 01:45:22 +0530

    2nd Problem

---
+++ pointer1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//Import the required library here
+#include <_______> 
+
+int main() {
+    //There are variables of 3 data types here 
+    int x;
+    double y;
+    char z;
+    
+    //Create pointer variables for each of them.
+    //Here is one example.
+    int* x_ptr = &x;
+    ________________
+    ________________
+    
+    //Print the memory addr. stored inside all the pointer variables you made.
+    //Here is one example.
+    printf("%p", x_ptr); 
+    ____________________
+    ____________________
+}
