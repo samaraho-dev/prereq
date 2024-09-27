beedfbf134d4d8fa0ed2e59c6d676760e6c0bcea
Samaraho Mukherjee <154082806+samaraho-dev@users.noreply.github.com>
2024-09-28 01:53:38 +0530

    4th Problem

---
+++ swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void swap(______, _____) {
+    int temp = ___;
+    ____ = ___;
+    ____ = ___;
+}
+
+int main() {
+    int x = 15;
+    int y = 20;
+    printf("Before %d %d\n", x,y);
+    swap(___, ___);
+    printf("After %d %d\n", x,y);
+}
