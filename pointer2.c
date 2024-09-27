93a5acf6ba6561e5ceefd0a82c0916dc0187968a
Samaraho Mukherjee <154082806+samaraho-dev@users.noreply.github.com>
2024-09-28 01:48:30 +0530

    3rd Problem

---
+++ pointer2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+//Import the required library
+
+int main() {
+    //There are variables of 3 data types here 
+    int x;
+    double y;
+    char z;
+    
+    //There are pointer variables containing their memory adresses
+    int *x_ptr;
+    double *y_ptr;
+    char *z_ptr;
+    
+    //Initialise them
+    ______
+    ______
+    ______
+    
+    //Print them
+    ____________
+    ____________
+    ____________
+}
